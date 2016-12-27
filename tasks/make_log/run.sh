56dfc4f20d348bbcc1f997a25edea09ac3dbaa77
vultr <root@vultr>
2016-12-28 00:37:03 +0800

    revise bug

--- tasks/make_log/run.sh
+++ tasks/make_log/run.sh
@@ -7,8 +7,7 @@ FREEWEB_TASK_PATH=$(cd `dirname $0`/../ ;pwd)
 # 更新数据库
 /usr/bin/python $FREEWEB_TASK_PATH/make_log/get_current_user_ip.py $FREEWEB_TASK_PATH/make_log/netstat.out >> $FREEWEB_TASK_PATH/../log/netstat.log
 
-
-
+rm $FREEWEB_TASK_PATH/make_log/netstat.out
 
 
 
--- tasks/run.sh
+++ tasks/run.sh
@@ -34,15 +34,15 @@ for path in `ls $FREEWEB_TASK_PATH/data/$PRE*.json`;do
         # 检查是否正确启动,写入数据库
         success_load=`pgrep -f "/usr/local/bin/ssserver -c $path" `
         if [ -n "$success_load" ] ; then
-            echo success:$path,pid=$success_load
+            echo `date` : SUCCESS:$path,pid=$success_load >>$FREEWEB_TASK_PATH/../log/start_server.log 
             state=1
         else
-            echo error:$path
+            echo `date` : ERROR:$path >>$FREEWEB_TASK_PATH/../log/start_server.log 
             state=-1
         fi
         mysql -u$SQL_USER -p$SQL_PASSWORD -Dweb_db -s -e "update ssserver set state=$state where port=${path:(-9):4};"
     else
-        echo pretend ssserver run: $path
+        echo  pretend ssserver run: $path
         mysql -u$SQL_USER -p$SQL_PASSWORD -Dweb_db -s -e "update ssserver set state=1 where port=${path:(-9):4};"
     fi
 done
@@ -51,22 +51,27 @@ done
 # 更新防火墙规则
 # 700/s 相当于1000KB/s
 if [ $1 -eq 0 ];then
-    iptables -F ssserver6
-    iptables -A ssserver6 -p tcp -m quota --quota $P_TOTAL_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
-    iptables -A ssserver6 -p tcp -j DROP
+    /sbin/iptables -F ssserver6
+    /sbin/iptables -A ssserver6 -p tcp -m quota --quota $P_TOTAL_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
+    /sbin/iptables -A ssserver6 -p tcp -j DROP
+    echo `date` : "flushed iptables ssserver6" >>$FREEWEB_TASK_PATH/../log/start_server.log 
 elif [  $1 -eq 1 ] ; then
-    iptables -F ssserver7
-    iptables -A ssserver7 -p tcp -m quota --quota $L_TOTAL_QUOTA  -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
-    iptables -A ssserver7 -p tcp -j DROP
+    /sbin/iptables -F ssserver7
+    /sbin/iptables -A ssserver7 -p tcp -m quota --quota $L_TOTAL_QUOTA  -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
+    /sbin/iptables -A ssserver7 -p tcp -j DROP
+    echo `date` : "flushed iptables ssserver7" >>$FREEWEB_TASK_PATH/../log/start_server.log 
 # 对于捐助用户，使用更宽泛的规则
 elif [ $1 -eq 2   ] ; then 
-    iptables -F ssserver8
+    /sbin/iptables -F ssserver8
     for path in `ls $FREEWEB_TASK_PATH/data/$PRE*.json`;do
-       iptables -A ssserver8 -p tcp --dport ${path:(-9):4} -m  quota --quota $D_PER_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
-       iptables -A ssserver8 -p tcp --sport ${path:(-9):4} -m  quota --quota $D_PER_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
-       iptables -A ssserver8 -p tcp --dport ${path:(-9):4} -j DROP
-       iptables -A ssserver8 -p tcp --sport ${path:(-9):4} -j DROP
+       /sbin/iptables -A ssserver8 -p tcp --dport ${path:(-9):4} -m  quota --quota $D_PER_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
+       /sbin/iptables -A ssserver8 -p tcp --sport ${path:(-9):4} -m  quota --quota $D_PER_QUOTA -m limit --limit 700/s --limit-burst 100 -j ACCEPT 
+       /sbin/iptables -A ssserver8 -p tcp --dport ${path:(-9):4} -j DROP
+       /sbin/iptables -A ssserver8 -p tcp --sport ${path:(-9):4} -j DROP
     done
+    echo `date` : "flushed iptables ssserver7" >>$FREEWEB_TASK_PATH/../log/start_server.log 
+else 
+    echo `date` : " ERROR flushed iptables" >>$FREEWEB_TASK_PATH/../log/start_server.log 
 fi
 
 
